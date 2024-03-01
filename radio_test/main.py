9c0bd49ca96e596b43a4a35556a4c25463b3fafe
Craptout <craptout@gmail.com>
2024-02-29 23:44:22 -0700

    Added A30 subclass

--- radio_test/main.py
+++ radio_test/main.py
@@ -6,8 +6,8 @@ variables
 ---------
 Pr = Power recieved
 Pt = Power transmitted
-Gt = Gain of transmitting antenna
-Gr = Gain of receiving antenna
+Gt = Gain of transmitting antenna = tx_ant_gain
+Gr = Gain of receiving antenna = rx_ant_gain
 d = separation between transmitting and receiving antennas
 Units for wavelength and distance must be the same. 
 SI Units are used for calculations and converted for users where needed.
@@ -21,11 +21,10 @@ frequecny = speed_of_light / wavelength
 
 import sys
 import math
-import pint
 from pint import UnitRegistry
 
 
-ureg = UnitRegistry(autoconvert_offset_to_baseunit=True)
+ureg = UnitRegistry(autoconvert_offset_to_baseunit=True) #baseunit required for dBm conversion
 # defines dBi and dBd
 ureg.load_definitions("my_def.txt")
 Q_ = ureg.Quantity
@@ -33,115 +32,108 @@ Q_ = ureg.Quantity
 c = Q_("299,792,458 m/s")
 
 
+# Q_ variables must be entered as valid pint Quantities! 
+    # Code assumes your implementation enforces this.
+    # Otherwise pint.DimentionalityError exception can be used.
 class RadioTest:
-    def __init__(self):
-        try:
-            self.freq = Q_(input("Enter Frequency: ")).to("Hz")
-        except pint.DimensionalityError:
-            print("You must enter units, e.g. Hz or MHz")
-            sys.exit(1)          
+    def __init__(self, separation, freq, tx_ant_gain, rx_ant_gain, power_transmitted):
+        self.separation = Q_(separation).to("m")
+        self.freq = Q_(freq).to("Hz")            
         self.wavelength = (c / self.freq).to("m") # Converts frequency to wavelength
-        self.far_field = 2 * self.wavelength        
-        # Comment out Gt, Gr, and Pt in loss and range functions to use these for A-30
-        self.Gt = Q_("2.15 dBi")  # Standard aviation antenna (0 dBd) 
-        self.Gr = Q_("2.15 dBi")  # Standard aviation antenna (0 dBd)
-        self.Pt = Q_("5 W")  # IA fire minimums (5W AM and 4.8W FM multiband)
-        self.accuracy = Q_("2 dB") # IFR 4000 RF level accuracy
+        self.far_field = 2 * self.wavelength             
+        self.tx_ant_gain = Q_(tx_ant_gain).to("dBi")
+        self.rx_ant_gain = Q_(rx_ant_gain).to("dBi")
+        self.power_transmitted = Q_(power_transmitted).to("dBm")
+        self.accuracy = Q_("2 dB") # IFR 4000 RF level accuracy. Provide setting/admin edit.
         self.uncertainty = Q_("1 dB") # Built in error to account for minor differences such as antenna placement
 
     def field(self):
-        try:
-            self.separation = Q_(
-                input("Enter distance between test set and radio antennas: ")
-            ).to("m")
-        except pint.DimensionalityError:
-            print("You must enter units, e.g. ft or meters")
-            self.field()        
-        # ensures antennas are in far field
-        if self.separation < self.far_field: 
-            print(
-                f"The test set is too close. The antennas must be greater than {self.far_field.to('ft'):.1f~P} ({self.far_field:.1f~P}) apart."
-            )
-            sys.exit(1)    
+        if self.separation < self.far_field:
+            print(f"The test set is too close. The antennas must be at least {self.far_field.to("ft"):.1f~P} ({self.far_field:.1f~P}) apart.")
+            sys.exit(1)
     
     def loss(self):
-        # comment out this try/except block to lock to 0 dBd for standard aviation antennas (A-30)
-        try:                
-            self.Gt = Q_(input("Enter gain of the transmitting antena: ")).to("dBi")
-            self.Gr = Q_(input("Enter gain of the receiving antenna: ")).to("dBi")
-        except pint.DimensionalityError:
-            print("You must enter units: dBi or dBd")                
-            self.loss()          
         self.path_loss = (
             (20 * math.log10(self.separation.magnitude))
             + (20 * math.log10(self.freq.magnitude))
             + (20 * (math.log10((4 * math.pi) / c.magnitude)))
-            - self.Gt.magnitude                
-            - self.Gr.magnitude                
+            - self.tx_ant_gain.magnitude                
+            - self.rx_ant_gain.magnitude                
         ) * ureg("dB")
         self.effective_loss = (self.path_loss.magnitude 
         + self.accuracy.magnitude 
         + self.uncertainty.magnitude) * ureg("dB")    
           
     def limits(self):
-        # comment out try/except block to lock at 5W for A-30 test
-        try:
-            self.Pt = Q_(input("Enter power of the transmited signal: ")).to("W")
-        except pint.DimensionalityError:
-            print("You must enter units, e.g. dBm or W")
-            self.limits()            
-        self.pr_24 = (
-            self.Pt.to("dBm").magnitude
-            + self.Gt.to("dBi").magnitude
-            + self.Gr.to("dBi").magnitude
+        self.radio_rf_24nmi = (
+            self.power_transmitted.magnitude
+            + self.tx_ant_gain.magnitude
+            + self.rx_ant_gain.magnitude
             + (20 * math.log10((self.wavelength.magnitude / (4 * math.pi * 44448))))
         ) * ureg(
             "dBm"
         )
-        self.pr_50 = (
-            self.Pt.to("dBm").magnitude
-            + self.Gt.to("dBi").magnitude
-            + self.Gr.to("dBi").magnitude
+        self.radio_rf_50nmi = (
+            self.power_transmitted.magnitude
+            + self.tx_ant_gain.magnitude
+            + self.rx_ant_gain.magnitude
             + (20 * math.log10((self.wavelength.magnitude / (4 * math.pi * 92600))))
         ) * ureg(
             "dBm"
         )  
-        # Minimum RF Level from test set needed to simulate 24 nmi range
-        self.rf_24 = (self.pr_24.magnitude + self.effective_loss.magnitude) * ureg("dBm")
-        # Minimum RF Level from test set needed to simulate 50 nmi range
-        self.rf_50 = (self.pr_50.magnitude + self.effective_loss.magnitude) * ureg("dBm")
-        # Display rf levels when separation is entered
-        print(
-            f"Test set RF Level must be at least {self.rf_24:.2f~P} for 24 nmi range"
-        )
-        print(
-            f"Test set RF Level must be at least {self.rf_50:.2f~P} for 50 nmi range"
-        )
-    
-    def range(self):        
-        try:
-            self.rf_level = Q_(input("Enter test set RF Level that opens receiver: ")).to("dBm")
-        except pint.DimensionalityError:
-            print("You must enter units, e.g. dBm or W")
-            self.range()
-        self.Pr = (self.rf_level.magnitude                            
+        self.test_set_rf_24nmi = (self.radio_rf_24nmi.magnitude + self.effective_loss.magnitude) * ureg("dBm")
+        self.test_set_rf_50nmi = (self.radio_rf_50nmi.magnitude + self.effective_loss.magnitude) * ureg("dBm")
+            
+    def range(self, test_set_rf):
+        self.test_set_rf = Q_(test_set_rf).to("dBm")       
+        self.power_received = (self.test_set_rf.magnitude                            
             - self.effective_loss.magnitude            
         ) * ureg("dBm")        
         self.radio_range = (
-            (math.pow(10, (self.Pt.to("dBm").magnitude 
-                            + self.Gt.magnitude 
-                            + self.Gr.magnitude 
-                            - self.Pr.magnitude                            
+            (math.pow(10, (self.power_transmitted.magnitude 
+                            + self.tx_ant_gain.magnitude 
+                            + self.rx_ant_gain.magnitude 
+                            - self.power_received.magnitude                            
                             + 180) 
                             / 20))
                             / (41.88 * self.freq.magnitude)
         ) * ureg("m")
         print(f"The radio's max effective range is {self.radio_range.to("nmi"):.2f~P}")
-        
+                
+# A30 locks parameters to comply with FS/OAS A-30 testing for wildland fire.
+# Frequency is locked and works throughout the AM and FM bands.
+    # Changes in range from frequency differences are offset by corresponding changes in path loss.
+class A30(RadioTest):
+    def __init__(self, separation, freq = "122.925 MHz", tx_ant_gain = "2.15 dBi", rx_ant_gain = "2.15 dBi", power_transmitted = "5 W"):
+        self.separation = Q_(separation).to("m")
+        self.freq = Q_(freq).to("Hz")            
+        self.wavelength = (c / self.freq).to("m") # Converts frequency to wavelength
+        self.far_field = 2 * self.wavelength             
+        self.tx_ant_gain = Q_(tx_ant_gain).to("dBi")
+        self.rx_ant_gain = Q_(rx_ant_gain).to("dBi")
+        self.power_transmitted = Q_(power_transmitted).to("dBm")
+        self.accuracy = Q_("2 dB") # IFR 4000 RF level accuracy. Provide setting/admin edit.
+        self.uncertainty = Q_("1 dB") # Built in error to account for minor differences such as antenna placement
     
-com1 = RadioTest()
+    def field(self):  # Required due to single freq for A30
+        if self.separation.magnitude < 6.095:
+            print("The test set is too close. The antennas must be at least 20 ft apart.")
+            sys.exit(1)
+  
+# A30 Example
+com1 = A30("20 ft")
 com1.field()
 com1.loss()
 com1.limits()
-com1.range()
+print(f"Test set RF Level must be at least {com1.test_set_rf_24nmi:.2f~P} for 50 nmi range")
+print(f"Test set RF Level must be at least {com1.test_set_rf_50nmi:.2f~P} for 50 nmi range")
+com1.range(input("Enter test set rf level: "))
 
+# RadioTest Example
+com2 = RadioTest(separation="30ft", freq="170 MHz", tx_ant_gain="0dBd", rx_ant_gain="0dBd",power_transmitted="5W")
+com2.field()
+com2.loss()
+com2.limits()
+print(f"Test set RF Level must be at least {com2.test_set_rf_24nmi:.2f~P} for 50 nmi range")
+print(f"Test set RF Level must be at least {com2.test_set_rf_50nmi:.2f~P} for 50 nmi range")
+com2.range(input("Enter test set rf level: "))
