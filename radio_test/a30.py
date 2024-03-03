690c3789909dc2379e84c4396f28e3d80ed36b57
Craptout <craptout@gmail.com>
2024-03-03 13:12:16 -0700

    Simplified A30 further

--- radio_test/a30.py
+++ radio_test/a30.py
@@ -7,22 +7,18 @@ import math
 
 
 class SimpleA30:
-    def __init__(self):
-        self.accuracy = 2  # Test set rf level accuracy in dBm. Set by admin.
-
-    def loss(self, separation):  # enter separation in feet
+    def __init__(self, separation):  # enter separation in feet
         self.separation = separation * 0.3048  # converts to meters for calculation
         if self.separation < 6.096:
             print("Separation must be 20 ft or greater")  # ensures far field
             sys.exit(1)
+        self.accuracy = 2  # Test set rf level accuracy in dBm. Set by admin.
         self.effective_loss = (
             ((20) * math.log10(self.separation))
             + 9.940587561127312
             + self.accuracy
             + 1  # 1 dBm uncertainty added to account for minor differences such as antenna placement
         )
-
-    def limits(self):
         # Minimum RF Level from test set needed to simulate 24 nmi range
         self.rf_24 = -65.9 + self.effective_loss
         # Minimum RF Level from test set needed to simulate 50 nmi range
@@ -36,9 +32,7 @@ class SimpleA30:
         ) * 0.00053996  # converts meters to nautical miles
 
 
-com1 = SimpleA30()
-com1.loss(20)
-com1.limits()
+com1 = SimpleA30(20)
 print(f"Test set RF Level must be at least {com1.rf_24:.2f} dBm for 24 nmi range")
 print(f"Test set RF Level must be at least {com1.rf_50:.2f} dBm for 50 nmi range")
 com1.range(float(input("Enter test set RF Level that opens receiver in dB: ")))
